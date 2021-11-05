368520223293027786df1ca5c78734f6045d0f0b
Raven <theravengrey@gmail.com>
2021-11-05 13:28:32 +0100

    Minor adjustments

--- src/components/JobsTimeline.js
+++ src/components/JobsTimeline.js
@@ -3,6 +3,40 @@
 import { Timeline, Text, Divider, Container, Grid, Col, InputWrapper, Progress, List, Space, Title } from '@mantine/core';
 import { CodeReviewIcon, CodeIcon, FlameIcon } from '@primer/octicons-react';
 
+const timeLine = [{
+    title: "Backend Developer",
+    icon: <CodeIcon size={12} />,
+    desc: "I was a part of a team of developers that were creating a mobile game streaming platform using NodeJS, WebRTC, AWS and Docker to just name a few of the things we worked with.",
+    period: "2020-01 - 2021-05"
+},
+{
+    title: "Fullstack Developer / IT Technician",
+    icon: <CodeReviewIcon size={12} />,
+    desc: "I worked as a fullstack developer / it technician managing virtual machines and hosting enviroments, I developed APIs and publicly available websites & I mostly used NodeJS, Express, React, MongoDB and MySQL.",
+    period: "2021-05 - 2021-08",
+    dashed: true
+}, {
+    title: "???",
+    icon: <FlameIcon size={12} />,
+    desc: "Currently looking for a new and exciting job!"
+}]
+
+const mySkills = [{
+    span: 12, language: "Javascript: 4 years", experience: 75, color: "yellow"
+}, {
+    span: 6, language: ".NET / C#: 3 years", experience: 60, color: "green"
+}, {
+    span: 6, language: "Lua: 3 years", experience: 60, color: "blue"
+}, {
+    span: 6, language: "Java: 1 year", experience: 40, color: "orange"
+}, {
+    span: 6, language: "PHP: 2 years", experience: 50, color: "indigo"
+}, {
+    span: 6, language: "C++: 1 year", experience: 30, color: "red"
+}, {
+    span: 6, language: "HTML / CSS: 2 years", experience: 60, color: "gray"
+}]
+
 const JobsTimeline = () => {
     return (
         <>
@@ -10,76 +44,38 @@ const JobsTimeline = () => {
                 <Title order={3}>Jobs:</Title>
                 <Space h="md"/>
                 <Timeline active={1} bulletSize={24} lineWidth={2} color="grape">
-                    <Timeline.Item bullet={<CodeIcon size={12} />} title="Backend Developer">
-                        <Text color="dimmed" size="sm">I was a part of a team of developers that were creating a mobile game streaming platform using NodeJS, WebRTC, AWS and Docker to just name a few of the things we worked with.</Text>
-                        <Text size="xs" style={{ marginTop: 4 }}>2020-01 - 2021-05</Text>
-                    </Timeline.Item>
-
-                    <Timeline.Item bullet={<CodeReviewIcon size={12} />} title="Fullstack Developer / IT Technician" lineVariant="dashed">
-                        <Text color="dimmed" size="sm">I worked as a fullstack developer / it technician managing virtual machines and hosting enviroments, I developed APIs and publicly available websites & I mostly used NodeJS, Express, React, MongoDB and MySQL.</Text>
-                        <Text size="xs" style={{ marginTop: 4 }}>2021-05 - 2021-08</Text>
-                    </Timeline.Item>
-
-                    <Timeline.Item title="???" bullet={<FlameIcon size={12} />}>
-                        <Text color="dimmed" size="sm">Currently looking for a new and exciting job!</Text>
-                    </Timeline.Item>
+                    {timeLine.map((job, index) => {
+                        return (
+                            <Timeline.Item bullet={job.icon} title={job.title} lineVariant={job.dashed ? "dashed" : "solid"} key={index}>
+                                <Text color="dimmed" size="sm">{job.desc}</Text>
+                                <Text size="xs" style={{ marginTop: 4 }}>{job.period}</Text>
+                            </Timeline.Item>
+                        )
+                    })}
                 </Timeline>
             </Container>
 
             <Divider my="xl"/>
 
-            <Container marginTop={10}>
+            <Container>
                 <Title order={3}>Skills:</Title>
                 <Space h="md"/>
                 <Grid>
-                    <Col span={12}>
-                        <InputWrapper label="Javascript: 4 years">
-                            <Progress size="xl" radius="md" value={75} color="yellow" striped />
-                        </InputWrapper>
-                    </Col>
-
-                    <Col span={6}>
-                        <InputWrapper label=".NET / C#: 3 years">
-                            <Progress size="xl" radius="md" value={60} color="green" striped />
-                        </InputWrapper>
-                    </Col>
-
-                    <Col span={6}>
-                        <InputWrapper label="Lua: 3 years">
-                            <Progress size="xl" radius="md" value={60} color="blue" striped />
-                        </InputWrapper>
-                    </Col>
-
-                    <Col span={6}>
-                        <InputWrapper label="Java: 1 year">
-                            <Progress size="xl" radius="md" value={40} color="orange" striped />
-                        </InputWrapper>
-                    </Col>
-
-                    <Col span={6}>
-                        <InputWrapper label="PHP: 2 years">
-                            <Progress size="xl" radius="md" value={50} color="indigo" striped />
-                        </InputWrapper>
-                    </Col>
-
-                    <Col span={6}>
-                        <InputWrapper label="C++: 1 year">
-                            <Progress size="xl" radius="md" value={30} color="red" striped />
-                        </InputWrapper>
-                    </Col>
-
-                    <Col span={6}>
-                        <InputWrapper label="HTML / CSS: 2 years">
-                            <Progress size="xl" radius="md" value={70} color="gray" striped />
-                        </InputWrapper>
-                    </Col>
-
+                    {mySkills.map((skill, index) => {
+                        return (
+                            <Col span={skill.span} key={index}>
+                                <InputWrapper label={skill.language}>
+                                    <Progress size="xl" radius="md" value={skill.experience} color={skill.color} striped />
+                                </InputWrapper>
+                            </Col>
+                        )
+                    })}
                 </Grid>
             </Container>
 
             <Divider my="xl"/>
 
-            <Container marginTop={10}>
+            <Container>
                 <Title order={3}>Experience:</Title>
                 <Space h="md"/>
                 <List>
--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,6 +1,44 @@
 import { TypographyStylesProvider, Tabs, Tab, Anchor } from "@mantine/core"
 import { useMediaQuery } from 'react-responsive';
 
+const SFRP = {
+    desc: [
+        `Back in 2016 I took over a small roleplaying server that was based on the FiveM framework for Grand Theft Auto: V, the community only had around 200 members at the time.`,
+
+        `With time I started to learn Lua and I started working on improving the server with faster MySQL queries, new fun activites for the players, new jobs and so on.
+        The server kept on growing in popularity and it happened FAST, the server was whitelisted and we would get around 100 applications each day that we had to read through carefully.`,
+
+        `In order for a player to apply for whitelist on the server he or she had to be over the age of 18 and have a detailed description about the character they want to play as.
+        If the application would be accepted we would then have a short interview with them so we could get an impression about the player and if they would fit in with us.`,
+
+        `Due to us having such a strict ruleset and the players keeping their roleplaying to a high quality we grew ever more,
+        we started to have a lot of streamers from Twitch.tv that streamed on our server and we would have around 600 people watching their streams in total each day.`,
+
+        `I ran the server for 4 years before I eventually had to shut it down as I could not maintain the server and have a normal job at the same time,
+        During that time I learned that managing such a popular and active server is like a full time job and I had to be available 24/7 in case of any issues.
+        We ended up with over 4000 registered members on our website and over 3000 members on our Discord server!`
+    ],
+    link: "https://swedishfamily.net",
+    buttonText: "Visit SWEDISHFAMILY"
+}
+
+const SnapTrap = {
+    desc: [
+        `I have always liked figuring out how things work and mess with things that I should probably not mess with.`,
+
+        `I was interested in Java and Android so I started a small project called SnapTrap which is esentially a small "hack" for an app called Snapchat.
+        The main function of SnapTrap is to "hook" into Snapchat and modify the app at runtime using a framework called: Xposed in order to save recived images and completely bypass its screenshot detection.`,
+
+        `Since the Snapchat APK is obfuscated I had to reverse engineer the APK in order to figure out where the function for the screenshot detection is located and then "hook" into it and replace that function with a empty return.
+        Usually on a non hooked version of Snapchat the user sending the image will get a notification if the receiving user screenshoted the image, SnapTrap completely disabled that function and can screenshot undetected.`,
+
+        `In order to save the images the user can either screenshot them or when opening Snapchat the project will navigate into the private folder of Snapchat and extract the cached images and convert them back into their normal file extension.
+        The project is made in React Native for the UI and in the back its using regular Java!`
+    ],
+    link: "https://snaptrap.io",
+    buttonText: "Visit SnapTrap"
+}
+
 const Projects = () => {
     const isMobile = useMediaQuery({ query: '(max-width: 760px)' })
 
@@ -8,57 +46,17 @@ const Projects = () => {
         <Tabs grow color="grape" variant="outline" orientation={isMobile ? "horizontal" : "vertical"}>
             <Tab label={isMobile ? "SFRP" : "SWEDISHFAMILY.NET"}>
                 <TypographyStylesProvider>
-                    <p>
-                        Back in 2016 I took over a small roleplaying server that was based on the FiveM framework for Grand Theft Auto: V, the community only had around 200 members at the time.
-                    </p>
+                    {SFRP.desc.map((content, index) => <p key={index}>{content}</p>)}
 
-                    <p>
-                        With time I started to learn Lua and I started working on improving the server with faster MySQL queries, new fun activites for the players, new jobs and so on.
-                        With time the server grew in popularity and it happened FAST, the server was whitelisted and we would get around 100 applications each day that we had to read through carefully.
-                    </p>
-
-                    <p>
-                        In order for a player to apply for whitelist on the server he or she had to be over the age of 18 and have a detailed description about the character they want to play as.
-                        If the application would be accepted we would then have a short interview with them so we could get an impression about the player and it he or she would fit in.
-                    </p>
-
-                    <p>
-                        Due to us having such a strict ruleset and the players keeping their roleplaying to a high quality we grew ever more,
-                        we started to have a lot of streamers from Twitch.tv that streamed on our server and we would have around 600 people watching their streams in total each day.
-                    </p>
-
-                    <p>
-                        I ran the server for 4 years before I eventually had to shut it down as I could not maintain the server and have a normal job at the same time,
-                        I learned that managing such a popular and active server is like a full time job and I had to be available 24/7 in case of any issues.
-                        We ended up with over 4000 registered members on our website and over 3000 members on our Discord server!
-                    </p>
-
-                    <Anchor href="https://swedishfamily.net" color="grape">Visit the SWEDISHFAMILY website!</Anchor>
+                    <Anchor href={SFRP.link} color="grape">{SFRP.buttonText}</Anchor>
                 </TypographyStylesProvider>
             </Tab>
 
             <Tab label="SnapTrap">
                 <TypographyStylesProvider>
-                    <p>
-                        I have always liked figure out how things work and mess with things that I should probably not mess with.
-                    </p>
+                    {SnapTrap.desc.map((content, index) => <p key={index}>{content}</p>)}
 
-                    <p>
-                        I was interested in Java and Android so I started a small project called SnapTrap which is esentially a small "hack" for an app called Snapchat.
-                        The main function of SnapTrap is to "hook" into Snapchat and modify the app at runtime using a framework called: Xposed in order to save recived images and completely bypass its screenshot detection.
-                    </p>
-
-                    <p>
-                        Since the Snapchat APK is obfuscated I had to reverse engineer the APK in order to figure out where the function for the screenshot detection is located and then "hook" into it and replace that function with a empty return.
-                        Usually on a non hooked version of Snapchat the user sending the image will get a notification if the receiving user screenshoted the image, SnapTrap completely disabled that function and can screenshot undetected.
-                    </p>
-
-                    <p>
-                        In order to save the images the user can either screenshot them or when opening Snapchat the project will navigate into the private folder of Snapchat and extract the cached images and convert them back into their normal file extension.
-                        The project is made in React Native for the UI and in the back its using regular Java!
-                    </p>
-
-                    <Anchor href="https://snaptrap.io" color="grape">Visit SnapTrap website!</Anchor>
+                    <Anchor href={SnapTrap.link} color="grape">{SnapTrap.buttonText}</Anchor>
                 </TypographyStylesProvider>
             </Tab>
         </Tabs>
